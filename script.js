e004705fb7687795c7f9724b87d49f46379ce250
Wilson Onazi <wonazi@yahoo.com>
2023-08-20 23:03:20 +0100

    feat:Add functionality that creates grid sizes based on user user selection
    Introduce "grid size" selection to the project using the "select" html element and javascript
    to update the grid.

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 //UI Nodes
 const gridContainer = document.querySelector(".grid");
+const gridSizes = document.querySelector(".grid-sizes");
 
 //Global Variables
-let gridSize = 16;
-const gridTiles = [];
-updateGrid(gridSize);
+
+
+//EventListeners
+gridSizes.addEventListener("change",updateGrid);
 
 //Functions 
-function updateGrid(gridSize){
-    let tileSize=640/gridSize;
+function updateGrid(e){
+    clearGrid();
+    const gridTiles = [];
+    const gridSize = parseInt(e.target.value);
+    const tileSize=640/gridSize;
 
     for (let i=0; i<Math.pow(gridSize,2);i++){
         const div = document.createElement("div");
         div.classList.add('tiles');
         div.setAttribute("style",`flex:0 0 1;width:${tileSize}px;height:${tileSize}px;`);
         gridContainer.appendChild(div);
-    }
-    
+    } 
 }
+
+function clearGrid(){
+    if(gridContainer.hasChildNodes()){
+        while(gridContainer.hasChildNodes()){
+            gridContainer.firstChild.remove();
+        };
+    }
+
+}
+
