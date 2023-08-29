a5ab5b24327353e264d5d4e2c26dcf5663eccf28
Wilson Onazi <wonazi@yahoo.com>
2023-08-29 20:40:34 +0100

    feat: Add grid colouring functionality
    Add several functions that enable grid tiles to be coloured (drawn on)
    using mounseclick and mouseover event listers.
    
    Notes: only colour black is avaiable for now. More color options to be added

--- script.js
+++ script.js
@@ -3,24 +3,34 @@ const gridContainer = document.querySelector(".grid");
 const gridSizes = document.querySelector(".grid-sizes");
 
 //Global Variables
-
-
+let gridSize;
+const penColor = "black";
+updateGrid(16);
 //EventListeners
 gridSizes.addEventListener("change",updateGrid);
+
 
 //Functions 
 function updateGrid(e){
     clearGrid();
-    const gridTiles = [];
-    const gridSize = parseInt(e.target.value);
+    if (typeof(e)==="number"){
+        gridSize = e;
+    }else{
+        gridSize = parseInt(e.target.value);
+    }
     const tileSize=640/gridSize;
-
     for (let i=0; i<Math.pow(gridSize,2);i++){
         const div = document.createElement("div");
-        div.classList.add('tiles');
+        div.classList.add('tile');
         div.setAttribute("style",`flex:0 0 1;width:${tileSize}px;height:${tileSize}px;`);
         gridContainer.appendChild(div);
     } 
+    const tiles = document.querySelectorAll(".tile");
+    tiles.forEach(tile => {
+        tile.addEventListener("mousedown",drawOnTile);
+        tile.addEventListener("mouseover",drawOnAdjacentTiles);
+    });
+
 }
 
 function clearGrid(){
@@ -32,3 +42,13 @@ function clearGrid(){
 
 }
 
+function drawOnTile(e){
+    e.target.style.backgroundColor=penColor;
+}
+
+function drawOnAdjacentTiles(e){
+    if (e.buttons===1){
+        console.log(e);
+        e.target.style.backgroundColor=penColor;
+    }
+}
